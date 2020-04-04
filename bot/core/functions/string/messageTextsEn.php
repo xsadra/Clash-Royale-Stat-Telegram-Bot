445fe8a768435cf2ec0cc423e314225c699ba4c6
Sadra <31988724+xsadra@users.noreply.github.com>
2020-04-04 18:28:38 +0200

    add all-bot-messages to the bot

---
+++ bot/core/functions/string/messageTextsEn.php
@@ -0,0 +1,63 @@
+<?php
+function getMsgNewUser() {
+    return "This Bot is for review Clash Royale Stat.
+
+Copy the #TAG and send it to the Bot. Bot will sends you your information in response.";
+}
+
+function getErrorMsg() {
+    return "Error - Checking the #TAG may be incorrect.";
+}
+
+function getNotValidUserReferenceErrorMessage() {
+    return "Your Reference User is not valid, check the link or just skip that.";
+}
+
+function getAddPointNotificationToUserReferenceMessage() {
+    return "You receive some point. Someone use your reference link to Start our BOT";
+}
+
+function getUserAlreadyExistMessage() {
+    return "Your ID already exists in our BOT";
+}
+
+function getReferenceIdAlreadyExistMessage() {
+    return "You already use This link.\nThis link is a disposable URL.";
+}
+
+function getRegisterSuccessMessage() {
+    return "Your registration has been successfully completed.";
+}
+
+function getInviteMessage() {
+    return "To see your Clash Royale Stat, join our Bot and enjoy it.";
+}
+
+function getLessPointNotificationMessage() {
+    return "Your current point is less than %s, invite someone to increase the point.";
+}
+
+function getPointNotificationMessage() {
+    return "Your current point is %s";
+}
+
+function getNoEnoughPointNotificationMessage() {
+    return "You don't have enough point to complete process, invite someone to increase the point.";
+}
+
+function getSuccessBlockedUserNotification() {
+    return "This User is Blocked.";
+}
+
+function getFailedBlockedUserNotification() {
+    return "You have to replay Message from User To Block.";
+}
+
+function getCurrentPointMessage() {
+    return "Current Points";
+}
+
+function getInviteLinkMessage() {
+    return "Get Invite Link";
+}
+
